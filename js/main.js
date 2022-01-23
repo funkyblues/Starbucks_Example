71607ef353987a35552a5824caab4de1127fd197
funkyblues <dnr8874@naver.com>
2022-01-23 23:27:08 +0900

    다중 요소 슬라이드

--- js/main.js
+++ js/main.js
@@ -87,6 +87,17 @@ new Swiper(".promotion .mySwiper", {
   },
 });
 
+new Swiper(".awards .mySwiper", {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: ".awards .swiper-prev",
+    nextEl: ".awards .swiper-next",
+  },
+});
+
 const promotionEl = document.querySelector(".promotion");
 const promotionToggleBtn = document.querySelector(".toggle-promotion");
 let isHidePromotion = false;
